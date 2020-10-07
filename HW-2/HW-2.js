70fd2eb19520531618d562ca87fa6a7c29e9d488
shangria <shangrialena@gmail,com>
2020-10-08 01:00:20 +0300

    HW-2

--- HW-2/HW-2.js
+++ HW-2/HW-2.js
@@ -13,15 +13,40 @@
 
 let input1 = prompt(' Введите число 1');
 let input2 = prompt(' Введите число 2');
-const result = getResult(Number(input1), Number(input2));
+const result = getResult(Number(parseInt(input1)), Number(parseInt(input2)));
 alert(result);
 
-// const userNumber1 = prompt('Введите число');
-// const userNumber2 = prompt('Введите число');
-//
-// const s =Math.round(userNumber1 / userNumber2) * userNumber2
-//
-// console.log(s)
+
+function getResult(input1, input2) {
+
+    if (isNaN(input1) || isNaN(input2)) {
+        alert('Введите число!!')
+    }
+
+    let integers = Math.floor(input1 / input2);
+    let candidate1 = (integers + 1) * input2;
+    let candidate2 = integers * input2;
+    let candidate1Diff = Math.abs(input1 - candidate1);
+    let candidate2Diff = Math.abs(input1 - candidate2);
+
+    let candidate1Obj = {value: candidate1, diff: candidate1Diff};
+    let candidate2Obj = {value: candidate2, diff: candidate2Diff};
+
+    let candidates = [candidate1Obj, candidate2Obj];
+    let result = null;
+    for (let candidate of candidates) {
+        if (candidate.value > 0) {
+            if (result == null || result.diff > candidate.diff) {
+                result = candidate;
+            }
+        }
+    }
+    if (result == null) {
+        return input2;
+    } else {
+        return result.value;
+    }
+}
 
 
 /*
@@ -76,32 +101,3 @@ function getResult(input1, input2) {
 //         return false;
 //     }
 // }
-
-
-function getResult(input1, input2) {
-    debugger;
-    let integers = Math.floor(input1 / input2);
-
-    let candidate1 = (integers + 1) * input2;
-    let candidate2 = integers * input2;
-    let candidate1Diff = Math.abs(input1 - candidate1);
-    let candidate2Diff = Math.abs(input1 - candidate2);
-
-    let candidate1Obj = {value: candidate1, diff: candidate1Diff};
-    let candidate2Obj = {value: candidate2, diff: candidate2Diff};
-
-    let candidates = [candidate1Obj, candidate2Obj];
-    let result = null;
-    for (let candidate of candidates) {
-        if (candidate.value > 0) {
-            if (result == null || result.diff > candidate.diff) {
-                result = candidate;
-            }
-        }
-    }
-    if (result == null) {
-        return input2;
-    } else {
-        return result.value;
-    }
-}
