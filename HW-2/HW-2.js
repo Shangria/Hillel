10de55b619555322c11ee66a71adbc8cebaedb99
shangria <shangrialena@gmail,com>
2020-10-10 13:40:46 +0300

    HW-2

--- HW-2/HW-2.js
+++ HW-2/HW-2.js
@@ -11,68 +11,97 @@
     Если пользователь ввёл невалидные данные - вместо ответа выводится сообщение об ошибке.*/
 
 
-
-const input1 = prompt(' Введите число 1');
-const input2 = prompt(' Введите число 2');
-const result = getResult(Number(parseInt(input1)), Number(parseInt(input2)));
-alert(result);
-
-
-
-function getResult(input1, input2) {
-
-    if (isNaN(input1) || isNaN(input2)) {
-        alert('Введите число!!')
-    }
-
-    const integers = Math.floor(input1 / input2);
-    const candidate1 = (integers + 1) * input2;
-    const candidate2 = integers * input2;
-    const candidate1Diff = Math.abs(input1 - candidate1);
-    const candidate2Diff = Math.abs(input1 - candidate2);
-
-    const candidate1Obj = {value: candidate1, diff: candidate1Diff};
-    const candidate2Obj = {value: candidate2, diff: candidate2Diff};
-
-    const candidates = [candidate1Obj, candidate2Obj];
-    let result = null;
-    for (let candidate of candidates) {
-        if (candidate.value > 0) {
-            if (result == null || result.diff > candidate.diff) {
-                result = candidate;
-            }
-        }
-    }
-    if (result == null) {
-        return input2;
+let input1 = parseInt(prompt(' Введите число 1'));
+if (isNaN(input1)) {
+    alert('input 1 is not a valid number');
+} else {
+    let input2 = parseInt(prompt(' Введите число 2'));
+    if (isNaN(input2)) {
+        alert('input 2 is not a valid number');
+    } else if (input2 <= 0) {
+        alert('input 2 must be greater than zero');
     } else {
-        return result.value;
+        const result = getResult(input1, input2);
+        alert(result);
     }
 }
 
 
-/*
 function getResult(input1, input2) {
-    if (input1 > 0 && input1 % input2 === 0) {
-        return input1;
+    if (input1 < 0) {
+        return input2;
     } else {
-        let step = 1;
-        while (true) {
-            let plus = input1 + step;
-            let minus = input1 - step;
-            let candidates = [plus, minus];
-            for (let candidate of candidates) {
-                if (candidate > 0 && candidate % input2 === 0) {
-                    console.log(`${candidate} is ok`);
-                    return candidate;
-                } else {
-                    console.log(`${candidate} is not ok`);
-                }
+        if (input1 % input2 === 0) {
+            return input1;
+        } else {
+            const rest = input1 / input2;
+            const nearest = Math.round(rest);
+            if (nearest === 0) {
+                return input2;
+            } else {
+                return nearest * input2;
             }
-            step++;
         }
     }
-}*/
+}
+
+
+// function getResult(input1, input2) {
+// debugger;
+//     if (isNaN(input1) || isNaN(input2)) {
+//         alert('Введите число!!')
+//
+//     }
+//
+//     const integers = Math.floor(input1 / input2);
+//     const candidate1 = (integers + 1) * input2;
+//     const candidate2 = integers * input2;
+//     const candidate1Diff = Math.abs(input1 - candidate1);
+//     const candidate2Diff = Math.abs(input1 - candidate2);
+//
+//     const candidate1Obj = {value: candidate1, diff: candidate1Diff};
+//     const candidate2Obj = {value: candidate2, diff: candidate2Diff};
+//
+//     const candidates = [candidate1Obj, candidate2Obj];
+//     let result = null;
+//     for (let candidate of candidates) {
+//         if (candidate.value > 0) {
+//             if (result == null || result.diff > candidate.diff) {
+//                 result = candidate;
+//             }
+//         }
+//     }
+//     if (result == null) {
+//         return input2;
+//     } else {
+//         return result.value;
+//     }
+// }
+
+
+// function getResult(input1, input2) {
+//     if (input1 > 0 && input1 % input2 === 0) {
+//         return input1;
+//     } else {
+//         let number = performance.now();
+//         let step = 1;
+//         while (true) {
+//             let plus = input1 + step;
+//             let minus = input1 - step;
+//             let candidates = [plus, minus];
+//             for (let candidate of candidates) {
+//                 if (candidate > 0 && candidate % input2 === 0) {
+//                     console.log(`${candidate} is ok`);
+//                     alert((performance.now() - number) + ' ms');
+//                     return candidate;
+//                 } else {
+//                     console.log(`${candidate} is not ok`);
+//                 }
+//             }
+//             step++;
+//         }
+//     }
+// }
 
 
 // function getResult(input1, input2) {
@@ -103,3 +132,5 @@ function getResult(input1, input2) {
 //         return false;
 //     }
 // }
+
+
