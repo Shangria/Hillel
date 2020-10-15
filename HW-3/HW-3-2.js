b96a2411bb485b663f7ce9f833758833dc01494b
shangria <shangrialena@gmail,com>
2020-10-16 01:09:31 +0300

    HW-3-2

--- HW-3/HW-3-2.js
+++ HW-3/HW-3-2.js
@@ -1,25 +1,47 @@
 // Напишите скрипт, который запрашивает у пользователя логин и пароль и работает по следующей логике.
 
+
 const userObj = {
     login: 'Alona',
     password: '1111'
 }
 const userLogin = prompt('Введите логин');
+let userPassword = prompt('Введите пароль');
+checkLogin(userLogin);
 
-if (userLogin === userObj.login) {
-
-    let userPassword = prompt('Введите пароль');
-
-    if (checkPassword(userPassword)) {
-        alert('Hi!');
-    }
-} else if (checkPassword(userPassword)) {
-    alert(`${userPassword} неверный!!`)
-    userPassword = prompt('Введите пароль');
-    checkPassword(userPassword);
-}
 
 function checkPassword(password) {
     if (password === userObj.password) {
+        console.log('Hi!');
+    } else if (password !== userObj.password) {
+        userPassword = prompt('Введите пароль еще раз');
+        checkPassword(userPassword);
+    } else {
+        console.log(`${userPassword} неверный!!`);
     }
 }
+
+function checkLogin(loginD) {
+    switch (loginD) {
+        case 'Alona':
+            checkPassword(userPassword);
+            return;
+        default:
+            console.log(`Логин ${loginD} неверный`);
+            return;
+    }
+}
+
+
+// второй вариант
+
+if (userObj.login !== prompt('Введите log')) {
+    alert('login not correct');
+} else if (userObj.password === prompt('Введите пароль')) {
+    alert('hi');
+} else if (userObj.password !== prompt('Введите пароль еще раз')) {
+    alert('password not correct');
+} else {
+    alert('Hi');
+}
+
