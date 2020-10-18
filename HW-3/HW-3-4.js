149aade020ca3878d143d0b67f3ff562e690e32c
shangria <shangrialena@gmail,com>
2020-10-18 13:04:40 +0300

    HW-3-4

--- HW-3/HW-3-4.js
+++ HW-3/HW-3-4.js
@@ -4,23 +4,15 @@
 // Напишите скрипт, который выводит все простые числа до введённого пользователем.
 
 function returnFibonacciNumber(number) {
-    let fibonacciNumber1 = 0;
+    let fibonacciNumber1 = 1;
     let fibonacciNumber2 = 1;
     let currentNumber = 1;
-    const arr = [];
     for (let i = 1; i <= number; i++) {
-        for (let j = 0; j < i; j++) {
-            if (!arr.includes(i)) {
-                arr.push(i);
-            }
-        }
         currentNumber = fibonacciNumber1 + fibonacciNumber2;
         fibonacciNumber1 = fibonacciNumber2;
         fibonacciNumber2 = currentNumber;
     }
-    const newArr = arr.slice(0, -1);
-    console.log(newArr);
-    return currentNumber;
+    return currentNumber
 }
 
 console.log(returnFibonacciNumber(prompt('Введите число')));
