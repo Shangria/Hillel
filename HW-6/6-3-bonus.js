aff28aa2c27057cbc0585f529d1380e2580754dc
shangria <shangrialena@gmail,com>
2020-11-01 12:48:14 +0200

    HW-6-1-2-3-bonus

--- HW-6/6-3-bonus.js
+++ HW-6/6-3-bonus.js
@@ -5,35 +5,50 @@
  Если пользователя с введённым логином не существует - скрипт предлагает создать такого пользователя с новым паролем, а затем предлагает залогиниться.
 */
 
+const arrObj = [];
 const user1 = {
     name: 'Vasya',
     password: 1111
-}
+};
+
+const user2 = {
+    name: 'Vasya',
+    password: 2222
+};
+
+arrObj.push(user1);
+arrObj.push(user2);
 
 const userLogin = prompt('Enter login');
 
 function checkUser(login) {
-    if (login === user1.name) {
-        let userChoice = confirm('This username already exists. Enter password')
-        while (userChoice) {
-            const password = prompt('Enter password');
-            password == user1.password ? userChoice = false : userChoice = true;
+    debugger
+    for (let value of arrObj) {
+        if (login === value.name) {
+            let userChoice = confirm('This username already exists. Enter password')
+            while (userChoice) {
+                const password = prompt('Enter password');
+                password == user1.password ? userChoice = false : userChoice = true;
+            }
+        } else {
+            createNewUser();
+            logIn();
         }
-    } else {
-        createNewUser();
-        logIn();
+
     }
+
 }
 
 checkUser(userLogin);
 
 function createNewUser() {
-    let newUserObj = user1;
+    let newUserObj = {};
     const newName = prompt('Enter new login');
     const newPassword = prompt('Enter new password');
 
     newUserObj.name = newName;
     newUserObj.password = newPassword;
+    arrObj.push(newUserObj);
 
     console.log(newUserObj)
     return newUserObj
