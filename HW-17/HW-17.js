c6c91d904171116793e4a49e6d7e22c709733c1e
shangria <shangrialena@gmail,com>
2020-11-13 23:20:13 +0200

    HW-16-17

--- HW-17/HW-17.js
+++ HW-17/HW-17.js
@@ -1,17 +1,12 @@
 // Напишите функцию, random(min, max), которая возвращает случайное целое число от min до max.
 
 (function () {
-    const userNumber1 = Number(prompt('Enter number'));
-    const userNumber2 = Number(prompt('Enter number'));
-
-    const minimum = Math.min(userNumber1, userNumber2);
-    const maximum = Math.max(userNumber1, userNumber2);
 
     function random(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-    random(minimum, maximum);
+    random(-4, 8);
 
 })();
 
