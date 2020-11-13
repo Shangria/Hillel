88c67c9b952220a81de6cc6d2bcfc372dc4889e2
shangria <shangrialena@gmail,com>
2020-11-13 23:01:17 +0200

    HW-16-17

--- HW-17/HW-17.js
+++ HW-17/HW-17.js
@@ -6,7 +6,6 @@
 
     const minimum = Math.min(userNumber1, userNumber2);
     const maximum = Math.max(userNumber1, userNumber2);
-    console.log(`min ${minimum} max ${maximum}`);
 
     function random(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
