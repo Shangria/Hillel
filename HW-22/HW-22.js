f187a6616751f08dbcf61b6cf8af303bff2eafbb
shangria <shangrialena@gmail,com>
2020-12-02 22:35:29 +0200

    HW-22

---
+++ HW-22/HW-22.js
@@ -0,0 +1,58 @@
+// Напишите вывод числа Фибоначчи по порядковому номеру двумя способами: рекурсией и циклом
+// row     => 1, 1, 2, 3, 5, 8, 13, 21
+// indexes => 1, 2, 3, 4, 5, 6,  7,  8
+
+function fibonachiIterative(index) {
+    if (index === 1) {
+        return 1;
+    } else if (index === 2) {
+        return 1;
+    } else {
+        let currentIndex = 2;
+        let previous = 1;
+        let current = 1;
+        while (index > currentIndex) {
+            let next = previous + current;
+            previous = current;
+            current = next;
+            currentIndex++;
+        }
+        return current;
+    }
+}
+
+function fibonachiRecursive(index) {
+    if (index < 2) {
+        return index
+    } else {
+        let previous = fibonachiRecursive(index - 1);
+        let prePrevious = fibonachiRecursive(index - 2);
+        return previous + prePrevious
+    }
+}
+
+// Напишите функцию, которая считает факториал числа рекурсивно и циклом
+function factorialIterative(number) {
+    if (number === 0) {
+        return 1;
+    } else {
+        let result = 1;
+        while (number > 1) {
+            result *= number;
+            number--;
+        }
+        return result;
+    }
+}
+
+function factorialRecursive(number) {
+    if (number === 0) {
+        return 1;
+    } else {
+        let result = number;
+        if (number > 1) {
+            result = number * factorialRecursive(--number);
+        }
+        return result;
+    }
+}
