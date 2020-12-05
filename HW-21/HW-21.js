63ef427badfec0382ca6b2f15278080ed870a5a6
shangria <shangrialena@gmail,com>
2020-12-05 21:11:45 +0200

    HW-21

---
+++ HW-21/HW-21.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.querySelector('form');
+    const inputText = form.querySelector('.user-text');
+    const buttonAdd = form.querySelector('.add');
+    const buttonDel = form.querySelector('.del');
+    const list = form.querySelector('ol');
+
+    buttonAdd.addEventListener('click', function (event) {
+        event.preventDefault();
+        const li = document.createElement('li');
+        li.classList.add('border');
+        li.innerText = inputText.value;
+        list.append(li);
+        inputText.value = '';
+
+        li.addEventListener('click', function () {
+            li.classList.toggle('add-background');
+        })
+    });
+
+    buttonDel.addEventListener('click', function (event) {
+        event.preventDefault();
+        document.querySelectorAll(".add-background").forEach(e => e.parentNode.removeChild(e));
+    });
+});
